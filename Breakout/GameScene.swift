2bfb544846da14fe3dbbee58dd68bd22a97baa63
Zuhair Ali <zuhairali20003@gmail.com>
2021-03-16 10:46:52 -0500

    Created ball object

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -10,9 +10,17 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var ball = SKShapeNode()
+    
     override func didMove(to view: SKView) {
         //This stuff happens once (When the app opens)
         createBackground()
+        resetGame()
+    }
+    
+    func resetGame() {
+        // this stuff happens before each game starts
+        makeBall()
     }
     
     func createBackground() {
@@ -29,4 +37,30 @@ class GameScene: SKScene {
             starsBackground.run(moveForever)
         }
 }
+    func makeBall() {
+        ball.removeFromParent() //remove the ball (if it exists)
+        ball = SKShapeNode(circleOfRadius: 10)
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
+        ball.strokeColor = .black
+        ball.fillColor = .yellow
+        ball.name = "ball"
+        
+        // physics shape matches ball image
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        // ignores all forces and impulses
+        ball.physicsBody?.isDynamic = false
+        // use precise collision detection
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        // no loss of energy from friction
+        ball.physicsBody?.friction = 0
+        // gravity is not a factor
+        ball.physicsBody?.affectedByGravity = false
+        // bounces fully off of other objects
+        ball.physicsBody?.restitution = 1
+        // does not slow down over time
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
+        
+        addChild(ball)  // add ball object to the view
+    }
 }
