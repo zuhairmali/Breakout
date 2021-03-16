0da6e4a35ecd5a043b0d53019b355afe899d8be1
Zuhair Ali <zuhairali20003@gmail.com>
2021-03-16 10:25:51 -0500

    Created Background

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -11,9 +11,22 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        //This stuff happens once (When the app opens) 
+        //This stuff happens once (When the app opens)
+        createBackground()
     }
     
-    
-    
+    func createBackground() {
+        let stars = SKTexture(imageNamed: "Stars")
+        for i in 0...1 {
+            let starsBackground = SKSpriteNode(texture: stars)
+            starsBackground.zPosition = -1
+            starsBackground.position = CGPoint(x: 0, y: starsBackground.size.height * CGFloat(i))
+            addChild(starsBackground)
+            let moveDown = SKAction.moveBy(x: 0, y: -starsBackground.size.height, duration: 20)
+            let moveReset = SKAction.moveBy(x: 0, y: starsBackground.size.height, duration: 0)
+            let moveLoop = SKAction.sequence([moveDown, moveReset])
+            let moveForever = SKAction.repeatForever(moveLoop)
+            starsBackground.run(moveForever)
+        }
+}
 }
