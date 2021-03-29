3ce52e41f0716ed0b298a16e783ce780ba201c4a
Zuhair Ali <zuhairali20003@gmail.com>
2021-03-29 10:36:26 -0500

    Setup game over routine

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -14,6 +14,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var paddle = SKSpriteNode()
     var brick = SKSpriteNode()
     var loseZone = SKSpriteNode()
+    var playLabel = SKLabelNode()
+    var livesLabel = SKLabelNode()
+    var scoreLabel = SKLabelNode()
+    var playingGame = false
+    var score = 0
+    var lives = 3
     
     override func didMove(to view: SKView) {
         //This stuff happens once (When the app opens)
@@ -22,7 +28,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         createBackground()
         makeLoseZone()
         resetGame()
-        kickBall()
+        makeLabels()
+        
     }
     
     func resetGame() {
@@ -30,11 +37,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         makeBall()
         makePaddle()
         makeBrick()
+        updateLabels()
     }
     
     func kickBall() {
         ball.physicsBody?.isDynamic = true
         ball.physicsBody?.applyImpulse(CGVector(dx: 3, dy: 5))
+    }
+    
+    func updateLabels() {
+        scoreLabel.text = "Score: \(score)"
+        livesLabel.text = "Lives: \(lives)"
     }
     
     func createBackground() {
@@ -107,10 +120,44 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(loseZone)
     }
     
+    func makeLabels() {
+        playLabel.fontSize = 24
+        playLabel.text = "Tap to Start"
+        playLabel.fontName = "Arial"
+        playLabel.position = CGPoint(x: frame.midX, y: frame.midY - 50)
+        playLabel.name = "PlayLabel"
+        addChild(playLabel)
+        
+        livesLabel.fontSize = 18
+        livesLabel.fontColor = .black
+        livesLabel.position = CGPoint(x: frame.minX + 50 , y: frame.minY + 10)
+        addChild(livesLabel)
+        
+        scoreLabel.fontSize = 18
+        scoreLabel.fontColor = .black
+        scoreLabel.position = CGPoint(x: frame.maxX - 50 , y: frame.minY + 10)
+        addChild(livesLabel)
+    }
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
-            paddle.position.x = location.x
+            if playingGame {
+                paddle.position.x = location.x
+            }
+            else {
+                for node in nodes(at: location) {
+                    if node.name == "playLabel" {
+                        playingGame = true
+                        node.alpha = 0
+                        score = 0
+                        lives = 3
+                        updateLabels()
+                        kickBall()
+                    }
+                }
+            }
         }
     }
     
@@ -124,14 +171,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func didBegin(_ contact: SKPhysicsContact) {
         if contact.bodyA.node?.name == "brick" ||
             contact.bodyB.node?.name == "brick" {
-            print("You Win!")
+            gameOver(winner: true)
             brick.removeFromParent()
             ball.removeFromParent()
         }
         if contact.bodyA.node?.name == "loseZone" ||
             contact.bodyB.node?.name == "loseZone" {
-            print("You Lose!")
+            gameOver(winner: false)
             ball.removeFromParent()
+        }
+    }
+    
+    func gameOver (winner: Bool) {
+        playingGame = false
+        playLabel.alpha = 1
+        resetGame()
+        if winner {
+            playLabel.text = "You win! Tap to play again."
+        }
+        else {
+            playLabel.text = "You lose! Tap to play again."
         }
     }
 }
